28f9d7112556dd2c1a584153b4f6474abcffd801
EssaouidiYassine <166022030+EssaouidiYassine@users.noreply.github.com>
2024-08-19 00:08:47 +0100

    Update main.js

--- main.js
+++ main.js
@@ -577,7 +577,7 @@ setInterval(async () => {
   if (stopped === 'close' || !conn || !conn.user) return;
   const _uptime = process.uptime() * 1000;
   const uptime = clockString(_uptime);
-  const bio = `[ ⏳ ] Uptime: ${uptime}`;
+  const bio = `تم تـطويـࢪ بـوت مـن طـࢪف السـويـدي يـاسـيـن[ ⏳ ] Uptime: ${uptime}`;
   await conn.updateProfileStatus(bio).catch((_) => _);
 }, 60000);
 function clockString(ms) {
